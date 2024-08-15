7bd8ff3cbd26c9e49aa4fe17dd3337b0712b110a
JoaoPedroFms <165235981+JoaoPedroFms@users.noreply.github.com>
2024-08-14 22:27:03 -0300

    Update App.java

--- src/App.java
+++ src/App.java
@@ -4,7 +4,7 @@ public class App {
         //------------------------------------------
         String expressao = LerEscrever.ler(); //Lê uma linha no terminal
 
-        
+        //chamar um método da classe Calcular que retorna a expressão connvertida e o resultado, passando "expressao" por parâmtro.
         
         LerEscrever.escrever(expressao); //imprime a string resultado
         //------------------------------------------
